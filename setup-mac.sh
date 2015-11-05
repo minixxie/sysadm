7344addc0150c84800ef0da57a45af3475a21116
Simon Tse <simon.tse@lalamove.com>
2015-11-05 11:57:49 +0800

    drafting a init script for mac os x

---
+++ setup-mac.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# allow other machine to ssh to this machine
+systemsetup -setremotelogin on
+
+# install brew - http://brew.sh/
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# ssh related tools
+brew install sshfs ssh-copy-id
+
+# install docker
+brew install docker boot2docker docker-compose docker-machine
+
+# install git
+brew install git Caskroom/cask/gitx
+brew install node npm #node.js
+
+#brew install mariadb
+#sudo apt-get -f install nginx
