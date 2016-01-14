e7fc9fce715db5c1f0fd8c02d77d40fdac1efe5e
Simon Tse <simon.tse@lalamove.com>
2016-01-14 18:55:26 +0800

    draft

--- installserver/mariadb.server.sh
+++ installserver/mariadb.server.sh
@@ -13,11 +13,15 @@ if [ x"$MYSQL_ROOT_PASS" != x ]
 then
 	mysqlRootPass="$MYSQL_ROOT_PASS"
 fi
+if [ x"$CONTAINER_NAME" != x ]
+then
+	CONTAINER_NAME="mariadb"
+fi
 
 
 section="# ----- [ MariaDB ] ----- #"
 echo "$section"
 sudo docker pull mariadb
-sudo docker run --name some-mariadb -e MYSQL_ROOT_PASSWORD=$mysqlRootPass -p 3306:3306 -d mariadb:latest
+sudo docker run --name $CONTAINER_NAME -e MYSQL_ROOT_PASSWORD=$mysqlRootPass -p 3306:3306 -d mariadb:latest
 sudo apt-get -q -y install mariadb-client-core-5.5 mariadb-client-5.5
 echo "use: \"mysql -h 127.0.0.1 -u root -p\" to force TCP/IP connection rather than UNIX domain socket"
